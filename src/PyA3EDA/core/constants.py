917b3f5b546ab5aa7cac2df79221846501019a2c
Markus-G-S-Weiss <114624948+Markus-G-S-Weiss@users.noreply.github.com>
2025-06-09 18:09:59 -0500

    Refactor constants for energy conversion factors and update mappings

--- src/PyA3EDA/core/constants.py
+++ src/PyA3EDA/core/constants.py
@@ -4,10 +4,13 @@ Constants Module
 Defines conversion factors and a mapping for sanitizing filenames.
 """
 class Constants:
-    HARTREE_TO_KJMOL = 2625.5002 # Hartree to kJ/mol
-    KJMOL_TO_KCALMOL = 1.0/4.184 # kJ/mol to kcal/mol
-    HARTREE_TO_KCALMOL = HARTREE_TO_KJMOL * KJMOL_TO_KCALMOL # Hartree to kcal/mol 627.5096080305926
+    HARTREE_TO_J=4.3597447222060e-18 # Hartree to kJ
     TO_KILO = 1e-3
+    HARTREE_TO_KJ = HARTREE_TO_J * TO_KILO # Hartree to kJ
+    AVOGADRO = 6.02214076e23 # Avogadro's number in mol^-1
+    HARTREE_TO_KJMOL = HARTREE_TO_KJ * AVOGADRO # Hartree to kJ/mol
+    KJMOL_TO_KCALMOL = 1.0/4.184 # kJ/mol to kcal/mol
+    HARTREE_TO_KCALMOL = HARTREE_TO_KJMOL * KJMOL_TO_KCALMOL # Hartree to kcal/mol
     ESCAPE_MAP = {
         ' ': '-space-', '(': '-lparen-', ')': '-rparen-',
         '[': '-lbracket-', ']': '-rbracket-', '{': '-lbrace-', '}': '-rbrace-',
